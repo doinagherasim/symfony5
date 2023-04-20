1cadb6b8ecfa5476506c4a0049957fa20fc0c502
Doina <gherasimdoina93@gmail.com>
2023-04-20 14:16:12 +0300

    symfony5-controller-php-class

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -3,10 +3,31 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class QuestionController
 {
-public function homepage() {
-    return new Response('new page');
-}
+    /**
+     * @Route ("/")
+     */
+    public function homepage() {
+        return new Response('Camelia is my favorite friend!');
+    }
+
+    /**
+     * @Route("/blog/new-page")
+     */
+
+    public function blog() {
+        return new Response('New page for my blog');
+    }
+
+    /**
+     * @Route("/blog/new-page/{slug}")
+     */
+
+    public function newpage($slug) {
+        return new Response(sprintf('New page for my blog "%s"!', $slug));
+    }
 }
