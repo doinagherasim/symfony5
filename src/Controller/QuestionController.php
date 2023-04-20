1c553abe9ab12cb1f0a42f317d1c463caf257fa7
Doina <gherasimdoina93@gmail.com>
2023-04-20 18:09:52 +0300

    symfony5-controller-php-class

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route ("/")
@@ -27,7 +28,22 @@ class QuestionController
      * @Route("/blog/new-page/{slug}")
      */
 
-    public function newpage($slug) {
-        return new Response(sprintf('New page for my blog "%s"!', $slug));
+//    public function newPage($slug) {
+//        return new Response(sprintf('New page for my blog "%s"!',
+//            ucwords(str_replace('-',' ', $slug))
+//        ));
+//    }
+
+    public function newPage ($slug)
+    {
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still ?',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+            ];
+    return $this->render('newPage.html.twig', [
+        'newPage' => ucwords(str_replace('-', ' ', $slug)),
+        'answers' => $answers,
+    ]);
     }
 }
